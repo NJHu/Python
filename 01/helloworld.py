6cf19fbc6f5dc2deda4a61ac2464a50a160fb7be
胡鹏 <64hp@163.com>
2019-12-12 19:45:47 +0800

    01

---
+++ 01/helloworld.py
@@ -0,0 +1,12 @@
+
+"""
+多行注释
+"""
+
+# 单行注释
+
+import sys
+print(sys.version_info)
+print(sys.version)
+print('hello world')
+
---
+++ 01/turtleP.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('blue')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(120)
+
+turtle.mainloop()
