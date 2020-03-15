4e5a6415c9dc1d478cdcac3686b6561255c9f1ff
胡鹏 <64hp@163.com>
2020-03-15 18:09:57 +0800

    绘制简单的图

--- Book/数据可视化/matplot01.py
+++ Book/数据可视化/matplot01.py
@@ -1,1 +1,18 @@
-import matplotlib
+import matplotlib.pyplot as pyplot
+
+def main():
+    input_values = [1, 2, 3, 4, 5]
+    squares = [1, 4, 9, 16, 25]
+    pyplot.plot(input_values, squares, lineWidth=5)
+    
+    pyplot.title("title", fontsize=24)
+    pyplot.xlabel("xLabel", fontsize=24)
+    pyplot.ylabel("yLabel", fontsize=24)
+    pyplot.tick_params(axis='both', labelsize=14)
+    
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
+    
