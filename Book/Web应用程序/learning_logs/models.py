9b9f2b22368706ca1aa4df964ace50bd275fc210
胡鹏 <64hp@163.com>
2020-03-19 22:06:52 +0800

    添加模型

--- Book/Web应用程序/learning_logs/models.py
+++ Book/Web应用程序/learning_logs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.text
