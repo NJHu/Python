5c7f79077d9f36eeb267f1c4ff7929e0912127ca
胡鹏 <64hp@163.com>
2020-03-22 12:55:40 +0800

    update

--- Book/Web应用程序/learning_logs/models.py
+++ Book/Web应用程序/learning_logs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Topic(models.Model):
-    text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.text
+    # text = models.CharField(max_length=200)
+    # date_added = models.DateTimeField(auto_now_add=True)
+    # def __str__(self):
+    #     return self.text
